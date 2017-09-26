bff54a08f2576abba003a182b66c8273c10704c2
v0vik <vovik_po@mail.ru>
2017-09-26 10:42:20 +0300

    tets - ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let reg = ''; 
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+
+// generate regular expression
+    let element = bracketsConfig[i].map((el) => {
+      let out;
+      switch (el) {
+        case '(':
+          out = '\\(';
+          break;
+        case ')':
+          out = '\\)';
+          break;
+        case '[':
+          out = '\\[';
+          break;
+        case ']':
+          out = '\\]';
+          break;
+        case '{':
+          out = '\\{';
+          break;
+        case '}':
+          out = '\\}';
+          break;
+        case '|':
+          out = '\\|';
+          break;
+
+        default: out = el;
+          break;
+      }
+
+      return out;
+    });
+
+    reg = reg + element[0] + '()*' + element[1] + '|';
+
+  }
+
+  reg = reg.slice(0, -1);
+  reg = new RegExp(reg);
+
+
+  while (str.search(reg) !== -1) {
+    str = str.replace(reg, '');
+  }
+
+  return str === '';
 }
